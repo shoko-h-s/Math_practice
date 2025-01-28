0c9bc07c1dfd06cc09202f61ce4b374b76a255c9
Shoko <mugwort.biwa@gmail.com>
2025-01-28 23:07:52 +0900

    Update prime_number_test_2.py

--- prime_number_test/prime_number_test_2.py
+++ prime_number_test/prime_number_test_2.py
@@ -1,6 +1,7 @@
-# 大きな数の素数判定
 # √nまでのループにするパターン
+# 1よりも計算量が減る
 
+# 平方根を用いるためにmathモジュールが必要
 import math
 
 n = int(input())
@@ -9,6 +10,7 @@ if n <= 1:
     print("NO")
 
 else:
+    # nの平方根をint型に修正する必要あり
     for i in range(2, int(math.sqrt(n))+1):
         if n % i == 0:
             print("NO")
